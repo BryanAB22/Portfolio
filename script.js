f75bbdca1fa80f53279a8b14f6b856a7bfbe6a79
Bryan Barrios <bryanbarrios@Bryans-MacBook-Pro.local>
2023-03-18 17:25:45 -0500

    Added new feature XYZ

---
+++ script.js
@@ -0,0 +1,41 @@
+const titles = [
+  {
+    lang: "en",
+    text: "Welcome To My Portfolio."
+  },
+  {
+    lang: "es",
+    text: "Bienvenido a Mi Portafolio."
+  },
+  {
+    lang: "fr",
+    text: "Bienvenue sur mon portfolio."
+  },
+  {
+    lang: "de",
+    text: "Willkommen auf meinem Portfolio."
+  },
+  {
+    lang: "it",
+    text: "Benvenuti nel mio portfolio."
+  },
+  {
+    lang: "zh",
+    text: "欢迎来到我的作品集。"
+  },
+  {
+    lang: "ja",
+    text: "私のポートフォリオへようこそ。"
+  }
+];
+
+let titleIndex = 0;
+const titleElement = document.querySelector("#Main h1");
+
+function changeTitle() {
+  titleElement.textContent = titles[titleIndex].text;
+  titleElement.lang = titles[titleIndex].lang;
+  titleIndex = (titleIndex + 1) % titles.length;
+}
+
+setInterval(changeTitle, 5000);
