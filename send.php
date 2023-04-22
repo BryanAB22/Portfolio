79a67eed9ff515af656ae177e1c662fb4bd9e048
Bryan Barrios <Bryanbarrios@gmail.com>
2023-04-22 15:16:24 -0500

    //change a couple info

--- send.php
+++ send.php
@@ -1,5 +1,5 @@
 <?php
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (isset($_SERVER["REQUEST_METHOD"]) && $_SERVER["REQUEST_METHOD"] == "POST") {
     $name = strip_tags(trim($_POST["name"]));
     $name = str_replace(array("\r","\n"),array(" "," "),$name);
     $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
